417548791b62f607ee10141da279ed2e0dec0a75
Akylbek <hsplay74@gmail.com>
2024-06-06 23:17:54 +0600

    added event handlers

--- src/components/UserFom/UserForm.tsx
+++ src/components/UserFom/UserForm.tsx
@@ -1,5 +1,44 @@
+import React, {useState} from "react";
+import {User, UserMute} from "../../type";
 
-const UserForm = () => {
+interface Props {
+    onSubmit: (user: User) => void;
+}
+
+const UserForm: React.FC<Props> = ({onSubmit}) => {
+
+    const [usersMute, setUsersMute] = useState<UserMute>({
+        name: '',
+        email: '',
+        status: false,
+        role: '',
+        id: ''
+    });
+
+    const changeUser = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setUsersMute((prevState) => ({
+                ...prevState,
+                [event.target.name]: event.target.value
+            }
+        ))
+    }
+
+    const onFormSubmit = (event: React.FormEvent) => {
+        event.preventDefault()
+
+        onSubmit ({
+            ...usersMute, id: Math.random().toString()
+        })
+
+        setUsersMute({
+            name: '',
+            email: '',
+            status: false,
+            role: '',
+            id: ''
+        })
+    }
+
     return (
         <form>
             <h4>Register new user!</h4>
@@ -11,6 +50,8 @@ const UserForm = () => {
                     id="name"
                     className="form-control"
                     required
+                    onChange={changeUser}
+                    value={usersMute.name}
                 />
             </div>
             <div className="form-group">
@@ -21,6 +62,8 @@ const UserForm = () => {
                     id="email"
                     className="form-control"
                     required
+                    onChange={changeUser}
+                    value={usersMute.email}
                 />
             </div>
             <div className="form-group my-1">
@@ -30,18 +73,20 @@ const UserForm = () => {
                     name="status"
                     id="status"
                     className="ms-1"
-                    required
+                    checked={usersMute.status}
+                    onChange={changeUser}
                 />
             </div>
             <div className="form-group">
                 <label htmlFor="role">Role</label>
-                <select name="role" id="role" className="ms-1">
+                <select name="role" id="role" value={usersMute.role} onChange={changeUser} className="ms-1">
+                    <option value="">Selecet</option>
                     <option value="user">User</option>
                     <option value="editor">Editor</option>
                     <option value="admin">Admin</option>
                 </select>
             </div>
-            <button type="submit" className="btn btn-dark px-3 mt-2 w-100">Save</button>
+            <button onClick={onFormSubmit} type="submit" className="btn btn-dark px-3 mt-2 w-100">Save</button>
         </form>
     );
 };
